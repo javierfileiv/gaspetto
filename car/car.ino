680eb2456be43bc7794e7011f2a1b6a73c2080c1
Javier Fileiv <javier.fileiv@qorvo.com>
2023-12-29 17:17:48 +0100

    car: clean program, motor 1 stopped working.
    To check what happened

--- car/car.ino
+++ car/car.ino
@@ -1,20 +1,13 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
 
-#define DC_MOTOR_PWM_FREQ	20000 /* 20 Khz PWM frequency. */
-#if defined(TIM1) // PB0 and PB1
-TIM_TypeDef *_timer1 = TIM1;
-#else
-#error "Choose another timer for your device"
-#endif
-
 #if defined(TIM2) // PB10 and PB11
 TIM_TypeDef *_timer2_ = TIM2;
 #else
 #error "Choose another timer for your device"
 #endif
 
-#if defined(TIM3) // PB4 and PB5
+#if defined(TIM3) // PB0 and PB1
 TIM_TypeDef *_timer3_ = TIM3;
 #else
 #error "Choose another timer for your device"
@@ -76,70 +69,62 @@ static void vPrintTask(void *pvParameters)
 	}
 }
 //------------------------------------------------------------------------------
-#define OPTION
-#ifdef OPTION
-const uint8_t IN_A = PB4;  // green  TIM3_CH1
-const uint8_t IN_B = PB5;  // orange   TIM3_CH2
-const uint8_t IN_C = PB3;  // violet   TIM2_CH2
-const uint8_t IN_D = PA15; // brown    TIM2_CH1_ETR
-#else
-const uint8_t IN_B = PB1; // TIM1_CH3N
-const uint8_t IN_A = PB0; // TIM1_CH2N
-const uint8_t IN_C = PB11; // TIM2_CH4
-const uint8_t IN_D = PB10; // TIM2_CH3
-#endif
 
-#define DUTY_A	0
-#define DUTY_B	0
-#define DUTY_C	50
+// #define TEST_FWD_BWD
+/* motor 1 is on the left with speed sensor down. */
+#define MOTOR_N 1
+#define DC_MOTOR_PWM_FREQ	30 /* 30 hz PWM frequency. */
+
+const uint8_t SENSOR_M1 = PC14;
+const uint8_t SENSOR_M2 = PC15;
+
+const uint8_t IN_A = PB11; // green  TIM2_CH4
+const uint8_t IN_B = PB10; // orange   TIM2_CH3
+const uint8_t IN_C = PB1; // violet   TIM3_CH4
+const uint8_t IN_D = PB0; // brown    TIM3_CH3
+
+#define DUTY_A	50
+#define DUTY_B	1
+#define DUTY_C	0
 #define DUTY_D	0
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin();
+
+	pinMode(SENSOR_M1, INPUT);
+	pinMode(SENSOR_M2, INPUT);
 
 	pinMode(IN_A, OUTPUT);
 	pinMode(IN_B, OUTPUT);
 	pinMode(IN_C, OUTPUT);
 	pinMode(IN_D, OUTPUT);
-#ifdef OPTION
-	TIM_TypeDef *Timer3 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_A), PinMap_PWM);
-	HardwareTimer *Timer3_instance = new HardwareTimer(Timer3);
-	TIM_TypeDef *Timer2 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_C), PinMap_PWM);
+
+#ifdef TEST_FWD_BWD
+	digitalWrite(IN_A, LOW);
+	digitalWrite(IN_B, LOW);
+	digitalWrite(IN_C, LOW);
+	digitalWrite(IN_D, LOW);
+#else
+
+	TIM_TypeDef *Timer2 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_A), PinMap_PWM);
 	HardwareTimer *Timer2_instance = new HardwareTimer(Timer2);
 
 	uint32_t channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_A), PinMap_PWM));
-	Timer3_instance->setPWM(channel, IN_A, DC_MOTOR_PWM_FREQ, DUTY_A);
+	Timer2_instance->setPWM(channel, IN_A, DC_MOTOR_PWM_FREQ, DUTY_A);
 
 	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_B), PinMap_PWM));
-	Timer3_instance->setPWM(channel, IN_B, DC_MOTOR_PWM_FREQ, DUTY_B);
+	Timer2_instance->setPWM(channel, IN_B, DC_MOTOR_PWM_FREQ, DUTY_B);
 
 
-	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_C), PinMap_PWM));
-	Timer2_instance->setPWM(channel, IN_C, DC_MOTOR_PWM_FREQ, DUTY_C);
-
-	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_D), PinMap_PWM));
-	Timer2_instance->setPWM(channel, IN_D, DC_MOTOR_PWM_FREQ, DUTY_D);
-#else
-	TIM_TypeDef *Timer1 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_B), PinMap_PWM);
-	HardwareTimer *Timer1_instance = new HardwareTimer(Timer1);
-
-	uint32_t channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_B), PinMap_PWM));
-	Timer1_instance->setPWM(channel, IN_B, 1000, 90); // 1 KHertz, 90% dutycycle
-
-	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_A), PinMap_PWM));
-	Timer1_instance->setPWM(channel, IN_A, 1000, 20); // 1 KHertz, 20% dutycycle
-
-
-	TIM_TypeDef *Timer2 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_C), PinMap_PWM);
-	HardwareTimer *Timer2_instance = new HardwareTimer(Timer2);
+	TIM_TypeDef *Timer3 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(IN_C), PinMap_PWM);
+	HardwareTimer *Timer3_instance = new HardwareTimer(Timer3);
 
 	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_C), PinMap_PWM));
-	Timer2_instance->setPWM(channel, IN_C, 5, 10); // 5 Hertz, 10% dutycycle
+	Timer3_instance->setPWM(channel, IN_C, DC_MOTOR_PWM_FREQ, DUTY_C);
 
 	channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(IN_D), PinMap_PWM));
-	Timer2_instance->setPWM(channel, IN_D, 5, 90); // 5 Hertz, 90% dutycycle
+	Timer3_instance->setPWM(channel, IN_D, DC_MOTOR_PWM_FREQ, DUTY_D);
 #endif
-
 	// create blink task
 	xTaskCreate(vLEDFlashTask,
 		    "Task1",
@@ -167,13 +152,10 @@ void setup()
 //------------------------------------------------------------------------------
 // WARNING idle loop has a very small stack (configMINIMAL_STACK_SIZE)
 // loop must never block
-
-//motor 1 est a la izquiera
-
-#define MOTOR_N 2
 void loop()
 {
 
+#ifdef TEST_FWD_BWD
 	// Full speed forward
 #if MOTOR_N==1
 	digitalWrite(IN_A, HIGH);
@@ -211,4 +193,7 @@ void loop()
 	digitalWrite(IN_D, LOW);
 
 	delay(5000);
+#else
+	delay(5000);
+#endif
 }
