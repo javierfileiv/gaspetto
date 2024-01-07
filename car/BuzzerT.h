6ebf88c66e3aa3a6000ed80d28da5763878181d5
Javier FILEIV <javier.fileiv@gmail.com>
2024-01-07 19:19:44 +0100

    car: add motor controller

---
+++ car/BuzzerT.h
@@ -0,0 +1,41 @@
+#ifndef BuzzerT_h
+#define BuzzerT_h
+
+#include "Arduino.h"
+
+class BuzzerT
+{
+private:
+public:
+	uint8_t Play;
+	HardwareTimer *TIMER;
+	uint32_t channel;
+	uint8_t buzzer_pin;
+	uint32_t frequency;
+
+	int *image[32][2];
+
+	uint8_t Pin;
+
+	unsigned long lastStart;
+	int Freq;
+	int Dura;
+
+	int lastFreq;
+	int lastConfig;
+	uint8_t counter;
+	uint8_t lastCounter;
+
+	uint32_t DutyCyc;
+	uint32_t PeriodCyc;
+	uint16_t Prescaler;
+	uint16_t Overflow;
+
+	BuzzerT(uint8_t Pin);
+	void Melody(int (&image)[32][2]);
+	void ConfigTimer(int Freq);
+	void DisableBuzzer();
+	void EnableBuzzer();
+};
+
+#endif
