fc9d58ab25e8ee0901f7424e53a72e9be06af7a0
Javier FILEIV <javier.fileiv@gmail.com>
2024-05-20 14:58:17 +0200

    nrfbridge: add NRF24L01 + Nano bridge using PC

---
+++ soft/NRFBridge/NRFBridge.ino
@@ -0,0 +1,122 @@
+#include "Timer.h"
+#include "globals.h"
+#include "stringstream.h"
+#include "types.h"
+#include "utils.h"
+#include <Arduino.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <printf.h>
+#include <stdint.h>
+#include <time.h>
+
+// https://github.com/bartoszbielawski/RFBridge/commits/master/src/main.cpp
+// for testing
+// picocom /dev/ttyUSB0 --baud 115200 --omap crcrlf --echo
+//
+#define VERSION "0.1.2"
+
+RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
+
+// configure behaviour of your system here:
+ReceiveMode receiveMode = ReceiveMode::Manual;
+TestMode testMode = TestMode::Disabled;
+rf24_pa_dbm_e defaultPower = RF24_PA_LOW;
+bool openPipesByDefault = false;
+uint8_t defaultAddress[5] = {0, 0, 0, 0, 0};
+
+Timer t;
+
+void testIrq() {
+  if (testMode != TestMode::Counter)
+    return;
+  char buffer[6] = {0};
+  snprintf(buffer, sizeof(buffer), "%04X", testCounter++);
+  radio.stopListening();
+  if (!radio.write(buffer, 5))
+    Serial.println("Error writing");
+  radio.startListening();
+}
+
+String command;
+
+void setup(void) {
+
+  command.reserve(72);
+  Serial.begin(115200);
+  printf_begin();
+  Serial.println("INFO: RF Bridge " VERSION);
+  radio.begin();
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware not responding!"));
+    while (1) {
+    } // hold program in infinite loop to prevent subsequent errors
+  }
+  radio.setPALevel(defaultPower);
+  if (openPipesByDefault) {
+    radio.openReadingPipe(1, defaultAddress);
+    radio.openWritingPipe(defaultAddress);
+  }
+  digitalWrite(LED_BUILTIN, 1);
+  t.every(1000, testIrq);
+}
+
+void loop(void) {
+  t.update();
+
+  switch (receiveMode) {
+  case ReceiveMode::Binary:
+    readDataBinary(false);
+    break;
+  case ReceiveMode::Hex:
+    readDataHex(false);
+    break;
+  default:;
+  }
+
+  if (!Serial.available()) {
+    return;
+  }
+
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+
+    if ((c != '\n') && (c != '\r')) {
+      command += c;
+      continue;
+    }
+
+    if (c == '\r')
+      continue;
+
+    if (command.length() == 0)
+      continue;
+
+    StringStream commandStream(command);
+    commandStream.setTimeout(0);
+    commandStream.discard(5);
+    String cmd = command.substring(0, 5);
+    cmd.toUpperCase();
+    command.toUpperCase();
+
+    Serial.println("searching...");
+    for (int i = 0;; ++i) {
+
+      Command commandStruct = {"", nullptr};
+      memcpy_P(&commandStruct, &command_list[i], sizeof(command_list[0]));
+      if (commandStruct.function == nullptr) {
+        Serial.print(F("CMD: Error, unknown command: "));
+        Serial.println(command);
+        break;
+      }
+
+      if (cmd == commandStruct.command) {
+        commandStruct.function(commandStream);
+        break;
+      }
+    }
+
+    command = "";
+  } // while available
+}
---
+++ soft/NRFBridge/globals.cpp
@@ -0,0 +1,31 @@
+#include "globals.h"
+#include "commands.h"
+#include <inttypes.h>
+
+
+uint16_t testCounter = 0;
+
+const Command command_list[] PROGMEM =
+{
+  {"TPOW?", reportTransmitPower, "Get TX power level"},
+  {"TPOW=", setTransmitPower,    "Set RX power level"},
+  {"PSIZ?", reportPayloadSize,   "Get payload size"},
+  {"PSIZ=", setPayloadSize,      "Set payload size"},
+  {"DATA?", isDataAvailable,     "Query RX data presence"},
+  {"PCLS=", closePipe,           "Close pipe"},
+  {"CARR?", isCarrierPresent,    "Query carrier presence"},
+  {"CHAN=", setChannel,          "Set channel"},
+  {"CHAN?", reportChannel,       "Get channel"},
+  {"RDDB?", readDataBinary,      "Read data (binary)"},
+  {"RDDX?", readDataHex,         "Read data (hex)"},
+  {"ARCE=", setAutoReceiveMode,  "Set autoreceive mode"},
+  {"ARCE?", reportAutoReceiveMode, "Get autoreceive mode"},
+  {"WRDB=", writeDataBinary,     "Write data (binary)"},
+  {"WRDH=", writeDataHex,        "Write data (hex)"},
+  {"POPN=", openPipe,            "Open pipe"},
+  {"OPEN=", open,                "Open pipes 0 and 1"},
+  {"HELP!", help,                "Help!"},
+  {"TEST=", setTestMode,         "Set test mode"},
+  {"TEST?", reportTestMode,      "Get test mode"},
+  {"ERROR", nullptr,             "No help :)"}
+};
