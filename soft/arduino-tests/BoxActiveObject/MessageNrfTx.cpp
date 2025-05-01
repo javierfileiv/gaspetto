9e5f9b71ed5235d69913eb5078c03104614589df
fixp <javier.fileiv@gmail.com>
2025-05-01 14:28:41 +0200

    arduino:remove useless examples

--- soft/arduino-tests/BoxActiveObject/MessageNrfTx.cpp
+++
@@ -1,7 +0,0 @@
-#include "MessageNrfTx.h"
-#include <string.h>
-
-MessageNrfTx::MessageNrfTx(const char* dataToSend) {
-  strncpy(data_, dataToSend, sizeof(data_) - 1);
-  data_[sizeof(data_) - 1] = '\0';
-}
