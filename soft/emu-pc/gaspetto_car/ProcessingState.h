007bd2a4788da7a57796906101be3f64d591fb65
Javier Fileiv <javier.fileiv@qorvo.com>
2025-05-16 07:39:47 +0200

    wip motor turning and speed sensor

--- soft/emu-pc/gaspetto_car/ProcessingState.h
+++ soft/emu-pc/gaspetto_car/ProcessingState.h
@@ -10,29 +10,5 @@ class Event;
 
 class ProcessingState : public State {
 public:
-    void enter() override;
     void processEvent(Event evt) override;
-
-private:
-    void moveMotor(float distance, bool forward);
-    void stopMotor(void);
-    /* Function to map duty cycle percentage to PWM value */
-    static int mapDutyCycle(int dutyCycle)
-    {
-        return map(dutyCycle, 0, 100, 0, 255);
-    }
-    void InitMotorPins(void);
-    void InitSpeedSensor(void);
-    void Init(void);
-
-public:
-    /* Constants for motor control. */
-    const int MOTOR_RIGHT_PIN_A = PB0; /* PWM pin for motor right. */
-    const int MOTOR_RIGHT_PIN_B = PB1; /* Direction pin for motor right. */
-    const int MOTOR_LEFT_PIN_A = PB11; /* Example PWM pin for motor left.  */
-    const int MOTOR_LEFT_PIN_B = PB10; /* Direction pin for motor left.  */
-    const int SPEED_SENSOR_LEFT_PIN = PA0; /* Pin for left speed/distance sensor. */
-    const int SPEED_SENSOR_RIGHT_PIN = PA1; /* Pin for right speed/distance sensor. */
-    /* Variables for motor control. */
-    bool initialized = false;
 };
