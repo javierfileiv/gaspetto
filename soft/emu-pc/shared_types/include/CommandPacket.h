858d38b55e7693d9509397ea221f5528d46a90a4
Javier FILEIV <javier.fileiv@gmail.com>
2026-07-16 16:03:30 +0200

    fixup! come back to old and works

--- soft/emu-pc/shared_types/include/CommandPacket.h
+++ soft/emu-pc/shared_types/include/CommandPacket.h
@@ -11,22 +11,22 @@ constexpr size_t BOX_LOOP_SLOTS = 6;
 constexpr size_t BOX_LED_SLOTS = 3;
 constexpr size_t BOX_MAX_PAYLOAD_COMMANDS = 31;
 
-// enum class BoxPieceId : uint8_t {
-//     EMPTY = 0,
-//     FORWARD,
-//     BACKWARD,
-//     TURN_RIGHT,
-//     TURN_LEFT,
-//     STOP,
-//     LOOP_CALL,
-//     INVALID,
-// };
+enum class BoxPieceId : uint8_t {
+    EMPTY = 0,
+    FORWARD,
+    BACKWARD,
+    TURN_RIGHT,
+    TURN_LEFT,
+    STOP,
+    LOOP_CALL,
+    INVALID,
+};
 
-// using BoxBoardPieces = BoxPieceId[BOX_TOTAL_SLOTS];
+using BoxBoardPieces = BoxPieceId[BOX_TOTAL_SLOTS];
 
-// struct __attribute__((packed)) CommandPacket {
-//     uint8_t count;
-//     uint8_t commands[BOX_MAX_PAYLOAD_COMMANDS];
-// };
+struct __attribute__((packed)) CommandPacket {
+    uint8_t count;
+    uint8_t commands[BOX_MAX_PAYLOAD_COMMANDS];
+};
 
-// static_assert(sizeof(CommandPacket) == 32, "CommandPacket must fit in a single nRF24 payload");
+static_assert(sizeof(CommandPacket) == 32, "CommandPacket must fit in a single nRF24 payload");
